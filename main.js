30009eb739867102a9e5a7c94a1e31f68b79b0af
jkcuk <44874423+jkcuk@users.noreply.github.com>
2025-03-02 16:03:58 +0000

    Added sharing functionality

--- main.js
+++ main.js
@@ -3,267 +3,273 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { edgesToCylinders } from './util.js';
 import { RainbowMaterial } from './rainbowMaterial.js';
+import { BasicApp } from './BasicApp.js';
 
 
-// parameters
-var omega = 2*2*Math.PI/1000;
-var wavenumber = 0;
-// var color = { r: 1, g: 1, b: 1 };
+class Opticklish extends BasicApp {
+    // parameters
+    omega = 2*2*Math.PI/1000;
+    wavenumber = 0;
+    // var color = { r: 1, g: 1, b: 1 };
 
-/**
- * rotationX: 0, rotationY: 1, rotationZ: 2, translationX: 3, translationY: 4, translationZ: 5, scaling: 6
- */
-var movementType = 1;   // rotationX: 0, rotationY: 1, rotationZ: 2, translationX: 3, translationY: 4, translationZ: 5, scaling: 6
-var movementSpeed = .5;
+    /**
+     * rotationX: 0, rotationY: 1, rotationZ: 2, translationX: 3, translationY: 4, translationZ: 5, scaling: 6
+     */
+    movementType = 1;   // rotationX: 0, rotationY: 1, rotationZ: 2, translationX: 3, translationY: 4, translationZ: 5, scaling: 6
+    movementSpeed = .5;
 
-/**
- * faces: 0, edges: 1
- */
-var meshType = 1;   // faces: 0, edges: 1
+    /**
+     * faces: 0, edges: 1
+     */
+    meshType = 1;   // faces: 0, edges: 1
 
-/**
- * 0: single color, 1: rainbow
- */
-var colorType = 0;  // 0: single color, 1: rainbow 
+    /**
+     * 0: single color, 1: rainbow
+     */
+    colorType = 0;  // 0: single color, 1: rainbow 
 
-/**
- * 0: torusKnot, 1: dodecahedron, 2: sphere, 3: cube
- */
-var geometryType = 0;   // 0: torusKnot, 1: dodecahedron, 2: sphere, 3: cube
+    /**
+     * 0: torusKnot, 1: dodecahedron, 2: sphere, 3: cube
+     */
+    geometryType = 0;   // 0: torusKnot, 1: dodecahedron, 2: sphere, 3: cube
 
-var edgeThickness = 0.005; // radius of the cylinders
+    edgeThickness = 0.005; // radius of the cylinders
 
-// Create scene
-var scene = new THREE.Scene();
-scene.background = 
-    new THREE.Color( 0x888888 );
-    // new THREE.Color( 0x0000ff );
+    // internal variables
+    material;
+    mesh;
+    scene;
+    matrix1;
+    matrix2;
+    matrix3;
+    deltaAngleMax = 1*Math.PI/180;
+    deltaShiftMax = 0.01;
+    deltaScaleMax = 0.01;
 
-// Create camera
-var camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+    /**
+	 * Represents a color material.
+	 * @constructor
+	 * @param {string} appName - The app's name
+	 * @param {string} appDescription - The app's (brief) description
+	 */
+	constructor(  ) {
+        super( 'Opticklish', 'the premier tool for optickle illusions' );
 
-// Create lights
-var lightAmb = new THREE.AmbientLight(0x777777);
-scene.add(lightAmb);
+        this.createRendererEtc();
 
-// Create renderer
-var renderer = new THREE.WebGLRenderer({ preserveDrawingBuffer: true, antialias: true });    // preserveDrawingBuffer is so that we can render several times without clearing
-renderer.autoClearColor = false;    
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.autoClear = false; // for rendering several times without clearing
-document.body.appendChild(renderer.domElement);
-
-let controls = new OrbitControls( camera, renderer.domElement );
-
-// add the scene objects
-
-var material;
-function reDefineMaterial() {
-    // switch(colorType) {
-    //     case 0:
-    //         material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-    //         break;
-    //     case 1:
-            material = new RainbowMaterial( colorType, 5, 5*2*Math.PI*wavenumber, 0 );
-    //         break;
-    // }
-    // new THREE.MeshLambertMaterial({
-    //     color: 0xf0f0f0,
-    //     // ambient: 0x121212,
-    //     emissive: 0xf121212
-    // });
-}
-reDefineMaterial();
-
-var mesh = new THREE.Mesh( new THREE.TorusGeometry(), material );
-function reDefineMesh() {
-    var geometry;
-    switch(geometryType) {
-        case 0:
-            geometry = new THREE.TorusKnotGeometry( 0.7, 0.2, 200, 16 );
-            break;
-        case 1:
-            geometry = new THREE.DodecahedronGeometry(1);
-            break;
-        case 2:
-            geometry = new THREE.SphereGeometry(1, 32, 32);
-            break;
-        case 3:
-            geometry = new THREE.BoxGeometry(1, 1, 1);
-            break;  
+        this.addGUI();
     }
 
-    switch(meshType) {
-        case 0:
-            mesh.geometry = geometry;
-            break;
-        case 1:
-            mesh.geometry = edgesToCylinders( new THREE.EdgesGeometry(geometry), edgeThickness );
-            break;
-    }
-}
-reDefineMesh();
-scene.add(mesh);
+    createRendererEtc() {
+        // Create scene
+        this.scene = new THREE.Scene();
+        this.scene.background = 
+            new THREE.Color( 0x888888 );
+            // new THREE.Color( 0x0000ff );
 
-// calculate the matrices
+        // Create camera
+        this.camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.position.z = 5;
 
-var matrix1, matrix2, matrix3;
-var deltaAngleMax = 1*Math.PI/180;
-var deltaShiftMax = 0.01;
-var deltaScaleMax = 0.01;
-function reCalculateMatrices() {
-    var forwardMatrix = new THREE.Matrix4();
-    switch( movementType ) {
-        case 0: // rotationX
-        forwardMatrix.makeRotationX( movementSpeed*deltaAngleMax );
-        break;
-        case 1: // rotationY
-        forwardMatrix.makeRotationY( movementSpeed*deltaAngleMax );
-        break;
-        case 2: // rotationZ
-        forwardMatrix.makeRotationZ( movementSpeed*deltaAngleMax );
-        break;
-        case 3: // translationX
-        forwardMatrix.makeTranslation(movementSpeed*deltaShiftMax, 0, 0);
-        break;
-        case 4: // translationY
-        forwardMatrix.makeTranslation(0, movementSpeed*deltaShiftMax, 0);
-        break;
-        case 5: // translationZ
-        forwardMatrix.makeTranslation(0, 0, movementSpeed*deltaShiftMax);
-        break;
-        case 6: // scaling
-        let scale = 1 + movementSpeed*deltaScaleMax;
-        forwardMatrix.makeScale(scale, scale, scale);
-        break;
-    }
+        // Create lights
+        var lightAmb = new THREE.AmbientLight(0x777777);
+        this.scene.add(lightAmb);
 
-    var backwardMatrix = forwardMatrix.clone().invert();
-    matrix2 = mesh.matrix.clone();
-    matrix1 = (new THREE.Matrix4()).multiplyMatrices(matrix2, forwardMatrix);
-    matrix3 = (new THREE.Matrix4()).multiplyMatrices(matrix2, backwardMatrix);
-}
-mesh.matrixAutoUpdate = false;
-reCalculateMatrices();
+        // add the object
+        this.reDefineMaterial();
 
-// Rendering function
+        this.mesh = new THREE.Mesh( new THREE.TorusGeometry(), this.material );
+        this.reDefineMesh();
+        this.scene.add(this.mesh);
 
-var lastDrawn = Date.now();
-var omegaT = 0;
-var phaseChangeForward = -0.2*2*Math.PI;
-
-var render = function () {
-    // calculate omega*t
-    let now = Date.now();
-    omegaT += omega*(now - lastDrawn);
-	lastDrawn = now;
-
-    renderer.clear();
-
-    // render three copies at different phases
+        this.mesh.matrixAutoUpdate = false;
+        this.reCalculateMatrices();
     
-    material.uniforms.omegaT.value = omegaT + phaseChangeForward;
-    mesh.matrix.copy(matrix1);
-    renderer.render(scene, camera);
+        // Create renderer
+        this.renderer = new THREE.WebGLRenderer({ preserveDrawingBuffer: true, antialias: true });    // preserveDrawingBuffer is so that we can render several times without clearing
+        this.renderer.autoClearColor = false;    
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.autoClear = false; // for rendering several times without clearing
+        document.body.appendChild(this.renderer.domElement);
 
-    material.uniforms.omegaT.value = omegaT - phaseChangeForward;
-    mesh.matrix.copy(matrix3);
-    renderer.render(scene, camera);
+        let controls = new OrbitControls( this.camera, this.renderer.domElement );
+    }
 
-    material.uniforms.omegaT.value = omegaT;
-    mesh.matrix.copy(matrix2);
-    renderer.render(scene, camera);
+    // add the scene objects
 
-    requestAnimationFrame( render );
-};
+    reDefineMaterial() {
+        this.material = new RainbowMaterial( this.colorType, 5, 5*2*Math.PI*this.wavenumber, 0 );
+    }
 
-requestAnimationFrame( render );
+    reDefineMesh() {
+        var geometry;
+        switch(this.geometryType) {
+            case 0:
+                geometry = new THREE.TorusKnotGeometry( 0.7, 0.2, 200, 16 );
+                break;
+            case 1:
+                geometry = new THREE.DodecahedronGeometry(1);
+                break;
+            case 2:
+                geometry = new THREE.SphereGeometry(1, 32, 32);
+                break;
+            case 3:
+                geometry = new THREE.BoxGeometry(1, 1, 1);
+                break;  
+        }
 
-// gui
+        switch(this.meshType) {
+            case 0:
+                this.mesh.geometry = geometry;
+                break;
+            case 1:
+                this.mesh.geometry = edgesToCylinders( new THREE.EdgesGeometry(geometry), this.edgeThickness );
+                break;
+        }
+    }
 
-const guiVariables = {
-	frequency: omega*1000/2/Math.PI,
-    wavenumber: wavenumber,
-	movementType: movementType,
-    movementSpeed: movementSpeed,
-    meshType: meshType,   // faces: 0, edges: 1
-    colorType: colorType,  // 1: rainbow, 0: single color
-    geometryType: geometryType, // 0: torusKnot, 1: dodecahedron
-    edgeThickness: edgeThickness,
-};
-	
-const gui = new GUI();
+    // calculate the matrices
 
-gui.add( guiVariables, 'frequency', -4, 4 )
-	.name( 'frequency (Hz)' )
-	.onChange( f => { 
-        omega = 2*Math.PI*f/1000; 
-} );
+    reCalculateMatrices() {
+        var forwardMatrix = new THREE.Matrix4();
+        switch( this.movementType ) {
+            case 0: // rotationX
+            forwardMatrix.makeRotationX( this.movementSpeed*this.deltaAngleMax );
+            break;
+            case 1: // rotationY
+            forwardMatrix.makeRotationY( this.movementSpeed*this.deltaAngleMax );
+            break;
+            case 2: // rotationZ
+            forwardMatrix.makeRotationZ( this.movementSpeed*this.deltaAngleMax );
+            break;
+            case 3: // translationX
+            forwardMatrix.makeTranslation(this.movementSpeed*this.deltaShiftMax, 0, 0);
+            break;
+            case 4: // translationY
+            forwardMatrix.makeTranslation(0, this.movementSpeed*this.deltaShiftMax, 0);
+            break;
+            case 5: // translationZ
+            forwardMatrix.makeTranslation(0, 0, this.movementSpeed*this.deltaShiftMax);
+            break;
+            case 6: // scaling
+            let scale = 1 + this.movementSpeed*this.deltaScaleMax;
+            forwardMatrix.makeScale(scale, scale, scale);
+            break;
+        }
 
-gui.add( guiVariables, 'wavenumber', 0, 1 )
-    .name( 'wavenumber' )
-    .onChange( k => {
-        wavenumber = k;
-        mesh.material.uniforms.k.value = 5*2*Math.PI*wavenumber;
-} );
+        var backwardMatrix = forwardMatrix.clone().invert();
+        this.matrix2 = this.mesh.matrix.clone();
+        this.matrix1 = (new THREE.Matrix4()).multiplyMatrices(this.matrix2, forwardMatrix);
+        this.matrix3 = (new THREE.Matrix4()).multiplyMatrices(this.matrix2, backwardMatrix);
+    }
 
-gui.add( guiVariables, 'movementType', { 'x rotation': 0, 'y rotation': 1, 'z rotation': 2, 'x translation': 3, 'y translation': 4, 'z translation': 5, scaling: 6 } )
-	.name( 'movement' )
-    .onChange( t => {
-	    movementType = t;
-	    reCalculateMatrices();
-} );
+    // Rendering function
 
-gui.add( guiVariables, 'movementSpeed', -1, 1 )
-	.name( 'relative speed' )
-	.onChange( a => { 
-        movementSpeed = a;
-        reCalculateMatrices();
-} );
+    lastDrawn = Date.now();
+    omegaT = 0;
+    phaseChangeForward = -0.2*2*Math.PI;
 
-gui.add( guiVariables, 'meshType', { 'faces': 0, 'edges': 1 } )
-	.name( 'show' )
-    .onChange( t => {
-	    meshType = t;
-        reDefineMesh();
-} );
+    render() {
+        // calculate omega*t
+        let now = Date.now();
+        this.omegaT += this.omega*(now - this.lastDrawn);
+        this.lastDrawn = now;
 
-gui.add( guiVariables, 'colorType', { 'single color': 0, 'rainbow': 1 } )
-	.name( 'colors' )
-    .onChange( t => {
-	    colorType = t;
-        reDefineMaterial();
-        mesh.material = material;
-} );
+        this.renderer.clear();
 
-gui.add( guiVariables, 'geometryType', { 'torus knot': 0, 'dodecahedron': 1, 'sphere': 2, 'cube': 3 } )
-    .name( 'geometry' )
-    .onChange( t => {
-        geometryType = t;
-        reDefineMesh();
-} );
+        // render three copies at different phases
+        
+        this.material.uniforms.omegaT.value = this.omegaT + this.phaseChangeForward;
+        this.mesh.matrix.copy(this.matrix1);
+        this.renderer.render(this.scene, this.camera);
 
-gui.add( guiVariables, 'edgeThickness', 0.001, 0.02 )
-    .name( 'edge thickness' )
-    .onChange( t => {
-        edgeThickness = t;
-        reDefineMesh();
-} );
+        this.material.uniforms.omegaT.value = this.omegaT - this.phaseChangeForward;
+        this.mesh.matrix.copy(this.matrix3);
+        this.renderer.render(this.scene, this.camera);
 
+        this.material.uniforms.omegaT.value = this.omegaT;
+        this.mesh.matrix.copy(this.matrix2);
+        this.renderer.render(this.scene, this.camera);
+    };
 
-// window resizing
+    // gui
 
-window.addEventListener("resize", onWindowResize, false);
+    guiVariables = {
+        frequency: this.omega*1000/2/Math.PI,
+        wavenumber: this.wavenumber,
+        movementType: this.movementType,
+        movementSpeed: this.movementSpeed,
+        meshType: this.meshType,   // faces: 0, edges: 1
+        colorType: this.colorType,  // 1: rainbow, 0: single color
+        geometryType: this.geometryType, // 0: torusKnot, 1: dodecahedron
+        edgeThickness: this.edgeThickness,
+    };
+    
+    addGUI() {
+        this.gui = new GUI();
 
-function onWindowResize() {
-	// in case the screen size has changed
-	if(renderer) renderer.setSize(window.innerWidth, window.innerHeight);
+        this.gui.add( this.guiVariables, 'frequency', -4, 4 )
+            .name( 'frequency (Hz)' )
+            .onChange( f => { 
+                this.omega = 2*Math.PI*f/1000; 
+        } );
 
-	// if the screen orientation changes, width and height swap places, so the aspect ratio changes
-	let windowAspectRatio = window.innerWidth / window.innerHeight;
-	camera.aspect = windowAspectRatio;
+        this.gui.add( this.guiVariables, 'wavenumber', 0, 1 )
+            .name( 'wavenumber' )
+            .onChange( k => {
+                this.wavenumber = k;
+                this.mesh.material.uniforms.k.value = 5*2*Math.PI*this.wavenumber;
+        } );
 
-    // make sure the camera changes take effect
-	camera.updateProjectionMatrix();
+        this.gui.add( this.guiVariables, 'movementType', { 'x rotation': 0, 'y rotation': 1, 'z rotation': 2, 'x translation': 3, 'y translation': 4, 'z translation': 5, scaling: 6 } )
+            .name( 'movement' )
+            .onChange( t => {
+                this.movementType = t;
+                this.reCalculateMatrices();
+        } );
+
+        this.gui.add( this.guiVariables, 'movementSpeed', -1, 1 )
+            .name( 'relative speed' )
+            .onChange( a => { 
+                this.movementSpeed = a;
+                this.reCalculateMatrices();
+        } );
+
+        this.gui.add( this.guiVariables, 'meshType', { 'faces': 0, 'edges': 1 } )
+            .name( 'show' )
+            .onChange( t => {
+                this.meshType = t;
+                this.reDefineMesh();
+        } );
+
+        this.gui.add( this.guiVariables, 'colorType', { 'single color': 0, 'rainbow': 1 } )
+            .name( 'colors' )
+            .onChange( t => {
+                this.colorType = t;
+                this.reDefineMaterial();
+                this.mesh.material = this.material;
+        } );
+
+        this.gui.add( this.guiVariables, 'geometryType', { 'torus knot': 0, 'dodecahedron': 1, 'sphere': 2, 'cube': 3 } )
+            .name( 'geometry' )
+            .onChange( t => {
+                this.geometryType = t;
+                this.reDefineMesh();
+        } );
+
+        this.gui.add( this.guiVariables, 'edgeThickness', 0.001, 0.02 )
+            .name( 'edge thickness' )
+            .onChange( t => {
+                this.edgeThickness = t;
+                this.reDefineMesh();
+        } );
+    }
 }
+
+let opticklish = new Opticklish();
+
+function render() {
+    opticklish.render();
+    requestAnimationFrame( render );
+}
+requestAnimationFrame( render );
